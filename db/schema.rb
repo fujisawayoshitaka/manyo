916dbc734e4ace72904cca88a0a91cde8aacd276
fujisawayoshitaka <1317699@gmail.com>
2020-01-07 13:04:19 +0900

    nullのマイグレーションファイル作る

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_034642) do
+ActiveRecord::Schema.define(version: 2020_01_07_035438) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
